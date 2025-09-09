675f4ab9afa0b969270d88e1ff43cdb6f02fecaf
bat1354 <151051272+bat1354@users.noreply.github.com>
2025-09-09 22:44:50 +0300

    Update Calculator.kt

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,17 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    val result = when (operation) {
+            Operation.ADD -> a + b
+            Operation.SUBTRACT -> a - b
+            Operation.MULTIPLY -> a * b
+            Operation.DIVIDE -> if (b != 0) a / b else null
+        }
+        result?.let {
+            println("Результат: $it")
+        } ?: println("Ошибка: деление на ноль")
+
+        return result
 }
 
 /**
